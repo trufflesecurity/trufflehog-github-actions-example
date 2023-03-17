797507f01d810b27ea93bc83a1504484e17130c6
Mike Vanbuskirk <mike.vanbuskirk@trufflesec.com>
2023-03-17 19:20:45 -0400

    initial commit of demo application

---
+++ s3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import boto3
+
+
+class S3BucketLister:
+    def __init__(self, region_name=None):
+        self.session = boto3.Session(region_name=region_name)
+        self.s3 = self.session.client("s3")
+
+    def list_buckets(self):
+        response = self.s3.list_buckets()
+        buckets = [bucket["Name"] for bucket in response["Buckets"]]
+        return buckets
+
+
+if __name__ == "__main__":
+    bucket_lister = S3BucketLister(region_name="us-east-1")
+    buckets = bucket_lister.list_buckets()
+    print(buckets)
